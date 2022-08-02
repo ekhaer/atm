f0878869d56f6e3f56438424aa8063db42f4eb5d
ekhaer <emiliakhaer@gmail.com>
2022-08-02 19:22:40 +0700

    init functionalities

--- account.js
+++ account.js
@@ -11,6 +11,14 @@ export class Account {
     return this.name;
   }
 
+  async getBalance() {
+    let read = await Filesystem.read(this.name);
+    if (isNaN(read)) {
+      this.balance = 0
+    } else {
+      this.balance = parseFloat(await Filesystem.read(this.name));
+    }
+  }
 
   static async find(username) {
     try {
@@ -18,6 +26,22 @@ export class Account {
     } catch (error) {
       return null;
     }
+  }
+
+  static async deposit(amount) {
+    console.log("deposit : ", amount)
+  }
+
+  static async withdraw(amount) {
+    console.log("withdraw")
+  }
+
+  static async transfer(amount, receiver) {
+    console.log("will transfer", amount, " to ", receiver )
+  }
+
+  static async createUser() {
+    console.log("createUser")
   }
 
 }
--- app.js
+++ app.js
@@ -9,16 +9,39 @@ async function main() {
   try {
     //account checking
     if(await Account.find(username) === null) {
-        //user is exist
-        console.log("user is not exist")
-    } else {
         //user is not exist
+        console.log("user is not exist")
+
+    } else {
+        //user is exist
         console.log("user is exist")
+        const account = new Account(username);
+        await account.getBalance();
+        machineAsk(account);
     }
   } catch (error) {
     console.log(error);
   }
 }
+
+async function machineAsk(account) {
+    const action = await Commandline.ask(
+      "What would you like to do?"
+    );
+    let input = action.split(" ");
+    let amount = parseFloat(input[1]);
+    let receiver = "";
+    if (action.includes("deposit")) {
+      await Account.deposit(amount);
+    } else if (action.includes("withdraw")) {
+      await Account.withdraw(amount);
+    } else if (action.includes("transfer")) {
+      amount = parseFloat(input[2]);
+      receiver = input[1];
+      await Account.transfer(amount, receiver);
+    }
+    // account.print();
+  }
 
 
 main();
